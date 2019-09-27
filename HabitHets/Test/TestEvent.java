8830dded60e2e2966458a4edf33d075f7b508548
Oscar Helgesson <oscar.ce.helgesson@gmail.com>
2019-09-27 12:48:08 +0200

    Completed all the first version logic for all the Event classes

--- HabitHets/Test/TestEvent.java
+++ HabitHets/Test/TestEvent.java
@@ -2,9 +2,11 @@ import main.model.EventHandler;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.time.LocalDateTime;
+
 public class TestEvent {
     @Test
-    public void testEvent(){
+    public void testEventId(){
         EventHandler eventHandler = EventHandler.getInstant();
         eventHandler.add();
         eventHandler.add();
@@ -16,5 +18,39 @@ public class TestEvent {
         Assert.assertEquals(2,eventHandler.getEventList().get(2).getId());
         Assert.assertEquals(3,eventHandler.getEventList().get(3).getId());
 
+    }
+
+    @Test
+    public void testEventModifiers(){
+        EventHandler eventHandler = EventHandler.getInstant();
+        eventHandler.add();
+        Assert.assertEquals(0, eventHandler.getEventList().get(0).getId());
+        eventHandler.getEventList().get(0).getId();
+        Assert.assertEquals("TestEvent", eventHandler.getEventList().get(0).getTitle());
+        eventHandler.getEventList().get(0).setTitle("NewName");
+        Assert.assertEquals("NewName", eventHandler.getEventList().get(0).getTitle());
+        Assert.assertEquals(1, eventHandler.getEventList().size());
+    }
+
+    @Test
+    public void testAddRemove(){
+        EventHandler eventHandler = EventHandler.getInstant();
+        eventHandler.add();
+        Assert.assertEquals(1,eventHandler.getEventList().size());
+        eventHandler.add();
+        Assert.assertEquals(2,eventHandler.getEventList().size());
+        eventHandler.remove(0);
+        Assert.assertEquals(1,eventHandler.getEventList().size());
+        Assert.assertEquals(1,eventHandler.getEventList().get(0).getId());
+        eventHandler.remove(5);
+        Assert.assertEquals(1,eventHandler.getEventList().size());
+    }
+
+    @Test
+    public void testTime(){
+        EventHandler eventHandler = EventHandler.getInstant();
+        eventHandler.add();
+        Assert.assertTrue((eventHandler.getEventList().get(0).getStartTime().isAfter(LocalDateTime.now())));
+        Assert.assertTrue((eventHandler.getEventList().get(0).getEndTime().isBefore(LocalDateTime.now().plusHours(1))));
     }
 }
