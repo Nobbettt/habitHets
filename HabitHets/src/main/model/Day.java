860c8294ee7bcf243694440b53b98788361c918e
MSNMessinger <Jacke1998>
2019-09-30 09:07:37 +0200

    Basic logic for Notes implemented

--- HabitHets/src/main/model/Day.java
+++ HabitHets/src/main/model/Day.java
@@ -4,8 +4,13 @@ import java.time.LocalDateTime;
 
 public class Day {
     private LocalDateTime ldt;
+
+
+
     public Day(LocalDateTime ldt) {
         this.ldt = ldt;
         System.out.println(ldt.getDayOfWeek());
+
+
     }
 }
---
+++ HabitHets/src/main/model/NoteHandler.java
@@ -0,0 +1,56 @@
+package main.model;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class NoteHandler implements IHandler {
+
+    public static NoteHandler instant;
+    public static List<Note> notesList;
+
+    private NoteHandler(){
+        this.notesList = new ArrayList<>();
+
+
+    }
+
+    public static NoteHandler getInstance(){
+        if(instant == null) {
+            instant = new NoteHandler();
+            return instant;
+
+        }else {
+            return instant;
+
+        }
+    }
+
+    public List<Note> getNotes(){
+        return notesList;
+    }
+
+    @Override
+    public void add() {
+        // Get input from interface
+    }
+
+    @Override
+    public void remove(int id) {
+        for(Note note : notesList){
+            if(note.getId() == id){
+                notesList.remove(note);
+                return;
+            }
+        }
+    }
+
+    public Note getNoteDate(LocalDateTime d){
+            for(Note n : notesList){
+                if(n.getDay().getDayOfYear() == d.getDayOfYear() && n.getDay().getYear() == d.getYear()){
+                    return n;
+                }
+        }return null;
+    }
+}
+
