8a417ef4be90fe90bc6de1fabcbc0c35e55e73ec
MSNMessinger <Jacke1998>
2019-10-16 17:57:57 +0200

    Added week and day labels for view

--- HabitHets/src/main/view/MonthView.java
+++ HabitHets/src/main/view/MonthView.java
@@ -7,9 +7,12 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import main.model.CalendarAble;
 import main.model.Day;
 
+import java.awt.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,7 +22,7 @@ public class MonthView extends StackPane implements ViewAble {
     @FXML private ScrollPane scrollPane;
     private List<Label> monthdays;
     private List<Label> weeknb;
-
+    private List<Label> weekday;
     public MonthView(){
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../resources/month.fxml"));
               fxmlLoader.setRoot(this);
@@ -33,23 +36,46 @@ public class MonthView extends StackPane implements ViewAble {
     }
 
      public void setupMonth() {
-        scrollPane.setFitToWidth(true);
-        scrollPane.setFitToHeight(true);
-         scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-         scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
          monthdays = new ArrayList<>();
          weeknb = new ArrayList<>();
-         for(int i = 0; i < 5; i++){
+         weekday = new ArrayList<>();
+         for(int i = 1; i < 7; i++){
              for(int j = 0; j < 8; j++){
                  AnchorPane a = new AnchorPane();
                  Label l = new Label("test");
                  a.getChildren().add(l);
 
+                 if(j == 0 && i == 1){
+                     l.setText("Week");
+                 }
+                 if(j == 1 && i == 1){
+                     l.setText("Mon");
+                 }
+                 if(j == 2 && i == 1){
+                     l.setText("Tue");
+                 }
+                 if(j == 3 && i == 1){
+                     l.setText("Wed");
+                 }
+                 if(j == 4 && i == 1){
+                     l.setText("Thu");
+                 }
+                 if(j == 5 && i == 1){
+                     l.setText("Fri");
+                 }
+                 if(j == 6 && i == 1){
+                     l.setText("Sat");
+                 }
+                 if(j == 7 && i == 1){
+                     l.setText("Sun");
+                 }
+
                  if(j == 0){
+                     l.setTextFill(Color.valueOf("#FF4500"));
                      weeknb.add(l);
                  }
                  else{
+                     l.setTextFill(Color.valueOf("#FFFF"));
                      monthdays.add(l);
                  }
 
