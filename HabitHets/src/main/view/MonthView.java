77bd8e64649575d875a138540df172f81064277d
MSNMessinger <Jacke1998>
2019-10-17 09:33:04 +0200

    Added methods to create days before and after the month we are on in order to make the view make sense. Finished the monthview.

--- HabitHets/src/main/view/MonthView.java
+++ HabitHets/src/main/view/MonthView.java
@@ -9,6 +9,7 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
+import main.model.Aggregate;
 import main.model.CalendarAble;
 import main.model.Day;
 
@@ -42,33 +43,8 @@ public class MonthView extends StackPane implements ViewAble {
          for(int i = 1; i < 7; i++){
              for(int j = 0; j < 8; j++){
                  AnchorPane a = new AnchorPane();
-                 Label l = new Label("test");
+                 Label l = new Label(" ");
                  a.getChildren().add(l);
-
-                 if(j == 0 && i == 1){
-                     l.setText("Week");
-                 }
-                 if(j == 1 && i == 1){
-                     l.setText("Mon");
-                 }
-                 if(j == 2 && i == 1){
-                     l.setText("Tue");
-                 }
-                 if(j == 3 && i == 1){
-                     l.setText("Wed");
-                 }
-                 if(j == 4 && i == 1){
-                     l.setText("Thu");
-                 }
-                 if(j == 5 && i == 1){
-                     l.setText("Fri");
-                 }
-                 if(j == 6 && i == 1){
-                     l.setText("Sat");
-                 }
-                 if(j == 7 && i == 1){
-                     l.setText("Sun");
-                 }
 
                  if(j == 0){
                      l.setTextFill(Color.valueOf("#FF4500"));
@@ -95,16 +71,48 @@ public class MonthView extends StackPane implements ViewAble {
     }
 
     private void updateMonthView(List<Day> month) {
-        int j = 0;
-        int w = month.get(0).getWeekNr()-4;
-        for (Label l : monthdays){
-            l.setText("" + month.get(0).getLdt().plusDays(j).getDayOfMonth() + "/" + month.get(0).getLdt().plusDays(j).getMonthValue());
-            if (j < month.size()-1)
-            j++;
+        Day firstday = (Day) month.get(0);
+        int k = firstday.getLdt().getDayOfWeek().getValue();
+
+
+        Aggregate a = new Aggregate();
+
+        List<Day> prevMonth = a.getMonthFromDate(firstday.getLdt().minusMonths(1));
+        int lastDayPrevMonth = prevMonth.size()-1;
+        for(int l = k; 0 <= l; l--){
+            Day tmpDay = (Day) prevMonth.get(lastDayPrevMonth);
+            Integer daynb = tmpDay.getLdt().getDayOfMonth();
+            monthdays.get(l).setText(daynb.toString());
+            lastDayPrevMonth--;
         }
+
+        int j = 0;
+        for(int i = k; i< monthdays.size(); i++){
+            if(j < month.size()){
+                Day tmpDay = (Day) month.get(j);
+                Integer daynb = tmpDay.getLdt().getDayOfMonth();
+                monthdays.get(i).setText(daynb.toString());
+                j++;
+            }else{
+                monthdays.get(i).setText(" ");
+            }
+
+        }
+
+        List<Day> nextMonth = a.getMonthFromDate(firstday.getLdt().plusMonths(1));
+        int firstDayNextMonth = 0;
+        for(int l = month.size()+k; l < monthdays.size(); l++){
+            Day tmpDay = (Day) nextMonth.get(firstDayNextMonth);
+            Integer daynb = tmpDay.getLdt().getDayOfMonth();
+            monthdays.get(l).setText(daynb.toString());
+            firstDayNextMonth++;
+        }
+
+        int w = month.get(0).getWeekNr()-4;
         for(Label l : weeknb){
             l.setText("" + w++);
         }
+
     }
     }
 
