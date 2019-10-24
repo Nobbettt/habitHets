e38a66035d02466acbe4ee2815f16ac31baebd88
MSNMessinger <Jacke1998>
2019-10-24 12:28:53 +0200

    Javadocsssss

--- HabitHets/src/main/model/Note.java
+++ HabitHets/src/main/model/Note.java
@@ -2,11 +2,23 @@ package main.model;
 
 import java.time.LocalDate;
 
+/**
+ * This class defines what a note is and all the variables a note needs.
+ */
 public class Note {
     private int id;
     private String description;
     private LocalDate day;
 
+    /**
+     * This is the only constructor of the Note class.
+     * It contains all of the instance variables. This makes
+     * sure that there is no need of adding exceptions to the class
+     * methods. (Exceptions that handles if a variable is not set).
+     * @param id
+     * @param description
+     * @param day
+     */
     Note(int id, String description, LocalDate day) {
         this.description = description;
         this.day = day;
