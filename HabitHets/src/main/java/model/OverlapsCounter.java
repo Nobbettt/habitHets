488059da0b6e9b3190dba636d13a0b34604d2b64
Norbert Laszlo <norbert.laszlo98@gmail.com>
2019-10-25 14:31:04 +0200

    devided larger method in overlapscounter into smaller more understanable methods

--- HabitHets/src/main/java/model/OverlapsCounter.java
+++ HabitHets/src/main/java/model/OverlapsCounter.java
@@ -8,6 +8,10 @@ import java.util.List;
  * Static class that counts number of overlaps given an id that used to find event
  */
 class OverlapsCounter {
+    private static List<Event> overlaps;
+    private static List<Event> overlaps2;
+    private static Interval interval;
+    private static double count;
 
     /**
      * Function calculates
@@ -16,33 +20,44 @@ class OverlapsCounter {
      * @return
      */
     static double countOverlaps(LocalDateTime dateTime, int id) {
-        double i = 0;
-        List<Event> overlaps = new ArrayList<>();
-        List<Event> overlaps2 = new ArrayList<>();
+        count = 0;
+        overlaps = new ArrayList<>();
+        overlaps2 = new ArrayList<>();
+        interval = new Interval(EventOrganizer.getEventOfId(id).getStartTime(), true, EventOrganizer.getEventOfId(id).getEndTime(), true);
+
         List<Event> events = EventOrganizer.getInstance().getEventsOfDay(dateTime);
-        Interval interval = new Interval(EventOrganizer.getEventOfId(id).getStartTime(), true, EventOrganizer.getEventOfId(id).getEndTime(), true);
+
+        firstCheck(events, id);
+        secondCheck(events, id);
+
+        events.add(EventOrganizer.getEventOfId(id));
+        return count;
+    }
+
+    private static void firstCheck(List<Event> events, int id) {
         for (Event tmpEvent : events) {
             Interval tmpInterval = new Interval(tmpEvent.getStartTime(), true, tmpEvent.getEndTime(), true);
             if (interval.overlaps(tmpInterval)) {
-                i++;
+                count++;
                 if (!overlaps.contains(tmpEvent) && !tmpEvent.equals(EventOrganizer.getEventOfId(id))) {
                     overlaps.add(tmpEvent);
                 }
             }
         }
+    }
+
+    private static void secondCheck(List<Event> events, int id) {
         for (Event event1 : overlaps) {
             Interval interval1 = new Interval(event1.getStartTime(), true, event1.getEndTime(), true);
             for (Event event2 : events) {
                 Interval interval2 = new Interval(event2.getStartTime(), true, event2.getEndTime(), true);
                 if (interval1.overlaps(interval2) && event2 != event1 && event2 != EventOrganizer.getEventOfId(id)) {
-                    i++;
+                    count++;
                     if (!overlaps2.contains(event2) && !event2.equals(event1)) {
                         overlaps2.add(event2);
                     }
                 }
             }
         }
-        events.add(EventOrganizer.getEventOfId(id));
-        return i;
     }
 }
